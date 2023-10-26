43150d3cf0320b77c3ee08f21238ee10e2691ca5
JohanniH <60876904+JohanniH@users.noreply.github.com>
2023-10-26 20:22:55 +0200

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+
+const root = document.getElementById("root");
+const app = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ReactDOM.render(app, root);
